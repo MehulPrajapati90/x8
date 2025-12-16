badc2622a336ced5b8813271773070aea80df8bc
Mehul Prajapati <ailearn90@gmail.com>
2025-12-16 19:20:49 +0530

    added onboarding-logic

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -1,8 +1,11 @@
+import { onBoardUser } from '@/actions/auth';
 import Navbar from '@/components/layout/navbar';
 import { Trees } from 'lucide-react';
 import React from 'react'
 
-const HomeLayout = ({ children }: { children: React.ReactNode }) => {
+const HomeLayout = async({ children }: { children: React.ReactNode }) => {
+    const user = await onBoardUser();
+    
     return (
         <div className='w-full min-h-screen flex flex-col justify-between items-center lg:flex-row auth-gradient'>
             <div className='w-full flex flex-col items-center min-h-screen'>
