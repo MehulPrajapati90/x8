4ac6eb434c7a79c02c602a9f7d4f1c0d37eebe75
Mehul Prajapati <ailearn90@gmail.com>
2026-01-02 00:10:14 +0530

    feat: fixed debouncing and custom shortening feature

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -20,7 +20,7 @@ const Navbar = () => {
                     <Link href='https://x.com/mehulHQ' target='_blank'>
                         <Twitter size={24} strokeWidth={1.5} className='transition-all duration-200 ease-in-out hover:text-[#ff0066]' />
                     </Link>
-                    <Link href='https://x.com/mehulHQ' target='_blank'>
+                    <Link href='https://github.com/MehulPrajapati90/x8' target='_blank'>
                         <Github size={24} strokeWidth={1.5} className='transition-all duration-200 ease-in-out hover:text-[#ff0066]' />
                     </Link>
                 </div>
